6ab785d4f417dbf9dae3f5bc0ca0bad2982c266f
Marcio Bera <marcio.bera@bauermedia.co.uk>
2022-03-29 14:50:31 +0100

    Change server listen door

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(9229, () => {
+server.listen(3000, () => {
     console.log('JSON Server is running')
 })
